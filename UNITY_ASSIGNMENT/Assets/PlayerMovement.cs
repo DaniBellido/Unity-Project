8bb88a661b3eafa3eb358cd9f0beae6058d8e586
DBTXkingston <77850087+DBTXkingston@users.noreply.github.com>
2022-01-04 19:22:07 +0000

    Character Controller and camera view
    The player can move using WASD keys and control camera view with Mouse. Character faces the mouse's direction.

---
+++ UNITY_ASSIGNMENT/Assets/PlayerMovement.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController controller;
+    public Transform cam;
+
+    public float speed = 6.0f;
+    public float turnSmoothTime = 0.1f;
+    float turnSmoothVelocity;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 diraction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        if (diraction.magnitude >= 0.1f) 
+        {
+            float targetAngle = Mathf.Atan2(diraction.x, diraction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            controller.Move(moveDirection.normalized * speed * Time.deltaTime);
+        }
+    }
+}
