27d48b34d41c9567dc0484cb3e4f011031e10d78
DBTXkingston <77850087+DBTXkingston@users.noreply.github.com>
2022-01-05 00:24:56 +0000

    Jump
    Player can jump. Gravity added.

--- UNITY_ASSIGNMENT/Assets/PlayerMovement.cs
+++ UNITY_ASSIGNMENT/Assets/PlayerMovement.cs
@@ -6,10 +6,18 @@ public class PlayerMovement : MonoBehaviour
 {
     public CharacterController controller;
     public Transform cam;
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
 
+    public float gravity = -9.81f;
+    public float jumpHeight = 3f;
     public float speed = 6.0f;
     public float turnSmoothTime = 0.1f;
     float turnSmoothVelocity;
+
+    Vector3 velocity;
+    bool isGrounded;
 
     // Update is called once per frame
     void Update()
@@ -26,6 +34,21 @@ public class PlayerMovement : MonoBehaviour
 
             Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
             controller.Move(moveDirection.normalized * speed * Time.deltaTime);
+        }
+
+        //Applying gravity to the player 
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && velocity.y < 0) 
+        {
+            velocity.y = -2f;
+        }
+
+        if (Input.GetButtonDown("Jump") && isGrounded) 
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2 * gravity);
         }
     }
 }
